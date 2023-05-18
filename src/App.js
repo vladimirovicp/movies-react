84ff45c2bb447de3160ef94bf199fb3be6e7c33a
vladimirovicp <vladimirovicp70@gmail.com>
2023-05-18 16:39:14 +0400

    add: 002 Подготовка проекта, создание шапки и подвала

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import React from "react";
+import {Header} from "./layout/Header";
+import {Footer} from "./layout/Footer";
+import {Main} from "./layout/Main"
+
 function App(){
     return (
-        <div className="App">
-            тест
-        </div>
+        <React.Fragment>
+            <Header />
+            <Main />
+            <Footer />
+        </React.Fragment>
     );
 }
 
