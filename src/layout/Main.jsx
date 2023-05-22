a8693c8768bea069df9836872d419f05b18157c6
vladimirovicp <vladimirovicp70@gmail.com>
2023-05-22 16:43:34 +0400

    add: Добавление строки поиска

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import {Movies} from "../components/Movies";
+import {Search} from "../components/Search";
+import {Preloader} from "../components/Preloader";
 
 
 const API_KEY = process.env.REACT_APP_API_KEY;
@@ -16,15 +18,22 @@ class Main extends React.Component{
             .then(data => this.setState({movies: data.Search}))
     }
 
+    searchMovies = (str) =>{
+        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}`)
+            .then(response => response.json())
+            .then(data => this.setState({movies: data.Search}))
+    }
+
     render() {
 
         const {movies} = this.state;
 
         return <main className='container content'>
+            <Search searchMovies={this.searchMovies} />
             {
                 movies.length ? (
                 <Movies movies={this.state.movies} />
-                ) : <h3>Loading...</h3>
+                ) : <Preloader />
             }
 
         </main>
