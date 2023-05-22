3ff6ad35c19e106bf336cc4d5b30a71c515b1fae
vladimirovicp <vladimirovicp70@gmail.com>
2023-05-22 15:12:19 +0400

    add: 005 Решение практики по созданию общего списка фильмов и 011 Безопасное хранение ключа API

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,8 +1,36 @@
-function Main(){
-    return <main className="container content">
-        Hello from Movies App
-    </main>
+import React from "react";
+import {Movies} from "../components/Movies";
+
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+class Main extends React.Component{
+
+    state = {
+        movies: [],
+    }
+
+    componentDidMount() {
+        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
+            .then(response => response.json())
+            .then(data => this.setState({movies: data.Search}))
+    }
+
+    render() {
+
+        const {movies} = this.state;
+
+        return <main className='container content'>
+            {
+                movies.length ? (
+                <Movies movies={this.state.movies} />
+                ) : <h3>Loading...</h3>
+            }
+
+        </main>
+    }
 }
+
 
 
 export {Main}
