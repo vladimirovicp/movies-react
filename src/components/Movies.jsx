5439b44b4fd030940e4141ae9e75d75635c04690
vladimirovicp <vladimirovicp70@gmail.com>
2023-05-31 12:08:49 +0400

    add: 010 Обработка неудачного поиска

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,15 +1,17 @@
 import {Movie} from './Movie'
 
 function Movies(props){
-    const {movies} = props;
+    const {movies = []} = props;
 
     return <div className="movies">
-        {
-            movies.map(movie => (
-                <Movie key={movie.imdbID} {...movie} />
-            )
-        )
-        }
+
+
+        { movies.length ? (
+            movies.map( (movie) => <Movie key={movie.imdbID} {...movie} />)
+            ) : (
+                <h4>Nothing found</h4>
+            )}
+
     </div>
 }
 
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -10,32 +10,35 @@ class Main extends React.Component{
 
     state = {
         movies: [],
+        loading: true,
     }
 
     componentDidMount() {
         fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
             .then(response => response.json())
-            .then(data => this.setState({movies: data.Search}))
+            .then(data => this.setState({movies: data.Search, loading: false}))
     }
 
     searchMovies = (str, type ='all') =>{
-        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+        this.setState({loading: true});
+        fetch(
+            `https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
+                type !== 'all' ? `&type=${type}` : ''
+            }`)
             .then(response => response.json())
-            .then(data => this.setState({movies: data.Search}))
+            .then(data => this.setState({movies: data.Search, loading: false}))
     }
 
     render() {
 
-        const {movies} = this.state;
+        const {movies, loading} = this.state;
 
         return <main className='container content'>
             <Search searchMovies={this.searchMovies} />
-            {
-                movies.length ? (
-                <Movies movies={this.state.movies} />
-                ) : <Preloader />
+            { loading ? (
+                <Preloader />
+                ) : <Movies movies={movies} />
             }
-
         </main>
     }
 }
